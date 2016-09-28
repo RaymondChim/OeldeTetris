95545cf5383f801ebbec4ac5494d9608e42947b9
everlastinggrey <rolf293stalker@gmail.com>
2016-09-28 14:29:41 +0300

    Add files via upload

---
+++ NAT/Services/CommonScoreService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Xml.Serialization;
+using System.IO;
+
+namespace NAT.Services {
+    public class CommonScoreService : IScoreService {
+
+        private const string SCORE_FILENAME = @"scores.xml";
+
+        public void AddScore(Tuple<string, int> score) {
+            var serializer = new XmlSerializer(typeof(Scores));
+            var readStream = new FileStream(SCORE_FILENAME, FileMode.OpenOrCreate);
+            var scores = serializer.Deserialize(readStream) as Scores;
+            readStream.Close();
+            var writeStream = new FileStream(SCORE_FILENAME, FileMode.Open);
+            scores.Heaver = scores.Heaver.Union(new ScoreHeaver[] { new ScoreHeaver(score.Item1,score.Item2) }).ToArray();
+            serializer.Serialize(writeStream,scores);
+            writeStream.Close();
+        }
+
+        public Scores GetScores() {
+            var serializer = new XmlSerializer(typeof(Scores));
+            return serializer.Deserialize(new FileStream(SCORE_FILENAME,FileMode.Open)) as Scores;
+        }
+    }
+}
---
+++ NAT/Services/IScoreService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Xml.Serialization;
+
+namespace NAT.Services {
+
+    [Serializable]
+    public class ScoreHeaver {
+        [XmlAttribute("Name")]
+        public string Name;
+        [XmlAttribute("Count")]
+        public int Score;
+
+        public ScoreHeaver(string _Name,int _Score) {
+            Score = _Score;
+            Name = _Name;
+        }
+
+        public ScoreHeaver() {}
+    }
+
+    [Serializable]
+    [XmlRoot("ScoreTable")]
+    public class Scores {
+        [XmlArray("Scores")]
+        [XmlArrayItem("Score")]
+        public ScoreHeaver[] Heaver;
+
+        public Scores() { }
+    }
+
+    interface IScoreService {
+        Scores GetScores();
+        void AddScore(Tuple<string, int> score);
+    }
+}
