e31b538707825c2568f928cdeb80b5243b1ac53f
everlastinggrey <rolf293stalker@gmail.com>
2016-09-30 13:18:22 +0300

    теперь не падает при отсутвии файла scores.xml ура

--- NAT/Services/CommonScoreService.cs
+++ NAT/Services/CommonScoreService.cs
@@ -13,22 +13,30 @@ namespace NAT.Services {
         private const string SCORE_FILENAME = @"scores.xml";
 
         public void AddScore(Tuple<string, int> score) {
-            var serializer = new XmlSerializer(typeof(Scores));
-            var readStream = new FileStream(SCORE_FILENAME, FileMode.OpenOrCreate);
-            var scores = serializer.Deserialize(readStream) as Scores;
-            readStream.Close();
-            var writeStream = new FileStream(SCORE_FILENAME, FileMode.Open);
-            scores.Heaver = scores.Heaver.Union(new ScoreHeaver[] { new ScoreHeaver(score.Item1,score.Item2) }).ToArray();
-            serializer.Serialize(writeStream,scores);
-            writeStream.Close();
+            try { 
+                var serializer = new XmlSerializer(typeof(Scores));
+                var readStream = new FileStream(SCORE_FILENAME, FileMode.OpenOrCreate);
+                var scores = serializer.Deserialize(readStream) as Scores;
+                readStream.Close();
+                var writeStream = new FileStream(SCORE_FILENAME, FileMode.Open);
+                scores.Heaver = scores.Heaver.Union(new ScoreHeaver[] { new ScoreHeaver(score.Item1,score.Item2) }).ToArray();
+                serializer.Serialize(writeStream,scores);
+                writeStream.Close();
+            }catch(Exception) {
+                // it is not ok guys
+            }
         }
 
         public Scores GetScores() {
-            var serializer = new XmlSerializer(typeof(Scores));
-            var stream = new FileStream(SCORE_FILENAME, FileMode.Open);
-            var scores = serializer.Deserialize(stream) as Scores;
-            stream.Close();
-            return scores;
+            try { 
+                var serializer = new XmlSerializer(typeof(Scores));
+                var stream = new FileStream(SCORE_FILENAME, FileMode.Open);
+                var scores = serializer.Deserialize(stream) as Scores;
+                stream.Close();
+                return scores;
+            }catch(Exception) {
+                return new Scores();
+            }
         }
     }
 }
